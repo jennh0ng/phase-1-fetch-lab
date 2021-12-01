e4bbea73d2966bf8ef3b4475e3f7581f1fa43205
Jennifer Hong <hongjenn24@gmail.com>
2021-12-01 15:04:43 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// fetch("URL")
+// calling fetch and passing a URL to a data source as the argument. This will return an object that represents what the data source sent back
+    // .then(function (response) {
+// call "then()" on the object that comes back from "fetch()" and capture the object into the response parameter so it can be passed as an argument in cb function
+    //  return response.json();
+    // })
+    // .then(function (json) {
+// use the data inside of `json` to do DOM manipulation
+    // });
+// the "then()" method receives the object returned from the first call to "then()" - pir "JSON-ified" object
+// pass it into a second cb function
+// "json" or "response" are often-used parameters for the cb function
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const response = fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json));
+  return response;
 }
 
 function renderBooks(books) {
